e512f85127a07311f24afa29c8506a62fda2a914
shdewz <shdewz@gmail.com>
2025-08-26 21:00:24 +0300

    Clean up

--- 4wc-nodecg-bundle/src/browser_shared/vue-replicants.ts
+++
@@ -1,5 +0,0 @@
-import { useReplicant as baseUseReplicant } from 'nodecg-vue-composable';
-import { namespace, replicantOptions, ReplicantTypes } from './replicants';
-
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion,import/prefer-default-export
-export const useReplicant = <N extends keyof ReplicantTypes, T extends ReplicantTypes[N]>(name: N) => baseUseReplicant<T>(name, namespace, replicantOptions[name])!;
